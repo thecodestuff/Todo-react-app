0fef2adbdaaa0ffdadcca888dae03c853b420eee
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2019-10-18 10:30:40 +0530

    added todoapp component,done basic styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
+import TodoApp from './TodoApp.js'
 import './App.css';
 
+class App extends Component{
+  render(){
+    return(
+      <div className="container">
+        <h1>Todo App</h1>
+        <TodoApp />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/TodoApp.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class TodoApp extends Component{
+  render(){
+    return(
+      <p>Your productive friend</p>
+    );
+  }
+}
+
+export default TodoApp;
